866779fb6abcff5dc4157cc83b972d4586a3cd1d
aliceclone <subject.edu@gmail.com>
2021-05-25 18:33:13 +0630

    no xml just config class

---
+++ spring-demo-annotation/src/config/demo/SpringConfig.java
@@ -0,0 +1,10 @@
+package config.demo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.spring.demo.annotation")
+public class SpringConfig {
+
+}
