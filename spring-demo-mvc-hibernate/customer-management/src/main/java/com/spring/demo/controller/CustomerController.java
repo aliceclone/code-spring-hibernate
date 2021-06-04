82e2c1c90fec23b37458a1523812fead1f34efce
aliceclone <subject.edu@gmail.com>
2021-06-04 21:33:31 +0630

    added spring-demo-mvc-hibernate

---
+++ spring-demo-mvc-hibernate/customer-management/src/main/java/com/spring/demo/controller/CustomerController.java
@@ -0,0 +1,16 @@
+package com.spring.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/customer")
+public class CustomerController {
+
+    @RequestMapping(value = "/list")
+    public String showList(Model model) {
+	return "customer-list";
+    }
+
+}
