0cb660cba715e281dc9fd70b3c712b1e1882436d
aliceclone <subject.edu@gmail.com>
2021-06-14 15:55:00 +0630

    updated for role

--- spring-demo-security/src/main/java/com/spring/demo/security/InMemoryCustomPageWebSecurityConfig.java
+++ spring-demo-security/src/main/java/com/spring/demo/security/InMemoryCustomPageWebSecurityConfig.java
@@ -1,6 +1,4 @@
-package com.spring.demo.security;
-
-//package com.spring.demo.config;
+//package com.spring.demo.security;
 //
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,7 +10,7 @@ package com.spring.demo.security;
 //
 //@Configuration
 //@EnableWebSecurity
-//public class InMemoryCustomFormWebSecurityConfig extends WebSecurityConfigurerAdapter {
+//public class InMemoryCustomPageWebSecurityConfig extends WebSecurityConfigurerAdapter {
 //
 //    @Override
 //    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -21,17 +19,37 @@ package com.spring.demo.security;
 //	UserBuilder users = User.withDefaultPasswordEncoder();
 //
 //	auth.inMemoryAuthentication().withUser(users.username("John").password("john123").roles("EMPLOYEE"))
-//		.withUser(users.username("Boss").password("boss123").roles("MANAGER"))
-//		.withUser(users.username("Mary").password("mary123").roles("ADMIN"));
+//		.withUser(users.username("Boss").password("boss123").roles("EMPLOYEE", "MANAGER"))
+//		.withUser(users.username("Mary").password("mary123").roles("EMPLOYEE", "ADMIN"));
 //
 //    }
 //
 //    // ❗"/auth" will handle by Spring, no controller + jsp need
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
-//
-//	http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login")
-//		.loginProcessingUrl("/auth").permitAll();
+//	http.authorizeRequests()
+//		// permit css, image etc. files
+//		.antMatchers("/resources/**").permitAll()
+//		// "/**" must be authorized
+//		// .anyRequest().authenticated()
+//		// permit to EMPLOYEE ROLE
+//		.antMatchers("/").hasAnyRole("EMPLOYEE")
+//		// permit to MANAGER ROLE
+//		.antMatchers("/managers/**").hasRole("MANAGER")
+//		// permit to SYSTEM ROLE
+//		.antMatchers("/systems/**").hasRole("ADMIN")
+//		//
+//		.and()
+//		// add custom login page, check authorization through "/auth"
+//		.formLogin().loginPage("/login").loginProcessingUrl("/auth").permitAll()
+//		//
+//		.and()
+//		// add logout
+//		.logout().permitAll()
+//		//
+//		.and()
+//		// custom page for access denied
+//		.exceptionHandling().accessDeniedPage("/access-denied");
 //    }
 //
 //}
