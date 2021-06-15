13a6e4ccb8d905fb22795b048b9e02a74aeabbea
aliceclone <subject.edu@gmail.com>
2021-06-15 20:17:59 +0630

    removed unnecessary files

--- spring-demo-REST/spring-demo-rest/src/main/java/com/spring/demo/entity/Student.java
+++ spring-demo-REST/spring-demo-rest/src/main/java/com/spring/demo/entity/Student.java
@@ -1,26 +1,13 @@
 package com.spring.demo.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-//@JsonIgnoreProperties(ignoreUnknown = true)
-// specific field
-@JsonIgnoreProperties(value = "hobby")
 public class Student {
 
-    private int id;
+    // private int id;
     private String firstName;
     private String lastName;
-    private boolean active;
-    private Address address;
-    private String[] skills;
-
-    public int getId() {
-	return id;
-    }
-
-    public void setId(int id) {
-	this.id = id;
-    }
+    // private boolean active;
+    // private Address address;
+    // private String[] skills;
 
     public String getFirstName() {
 	return firstName;
@@ -36,30 +23,6 @@ public class Student {
 
     public void setLastName(String lastName) {
 	this.lastName = lastName;
-    }
-
-    public boolean isActive() {
-	return active;
-    }
-
-    public void setActive(boolean active) {
-	this.active = active;
-    }
-
-    public Address getAddress() {
-	return address;
-    }
-
-    public void setAddress(Address address) {
-	this.address = address;
-    }
-
-    public String[] getSkills() {
-	return skills;
-    }
-
-    public void setSkills(String[] skills) {
-	this.skills = skills;
     }
 
 }
