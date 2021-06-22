dcc325f96749b22c57a5976139bc245ba9d4c90c
aliceclone <subject.edu@gmail.com>
2021-06-22 23:41:00 +0630

    removed hibernate entity

--- spring-demo-crud/web-app-rest-client/crm-webapp-client/src/main/java/com/spring/demo/entity/Customer.java
+++ spring-demo-crud/web-app-rest-client/crm-webapp-client/src/main/java/com/spring/demo/entity/Customer.java
@@ -1,32 +1,18 @@
 package com.spring.demo.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "customer")
 public class Customer {
 
-    public Customer() {
-    }
-
-    @Id
-    @Column
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "first_name")
     private String firstName;
 
-    @Column(name = "last_name")
     private String lastName;
 
-    @Column
     private String email;
+
+    public Customer() {
+
+    }
 
     public int getId() {
 	return id;
