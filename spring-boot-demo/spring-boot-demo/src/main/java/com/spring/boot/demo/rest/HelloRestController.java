5da88b67e6c2cddb83d1c9f68691147ebe1c4038
aliceclone <subject.edu@gmail.com>
2021-06-24 02:51:39 +0630

    test some props

--- spring-boot-demo/spring-boot-demo/src/main/java/com/spring/boot/demo/rest/HelloRestController.java
+++ spring-boot-demo/spring-boot-demo/src/main/java/com/spring/boot/demo/rest/HelloRestController.java
@@ -2,15 +2,20 @@ package com.spring.boot.demo.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRestController {
 
+    // inject from application.properties
+    @Value("${greet.default}")
+    private String greet;
+
     @GetMapping("/")
     public String greet() {
-	return "hello " + LocalDateTime.now();
+	return greet + " " + LocalDateTime.now();
     }
 
 }
