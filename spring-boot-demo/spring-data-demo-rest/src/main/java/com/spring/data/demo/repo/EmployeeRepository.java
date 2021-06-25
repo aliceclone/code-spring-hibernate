d13fe949cf6dd23cc5779fc92315ee8cfc2c94a6
aliceclone <subject.edu@gmail.com>
2021-06-26 01:40:29 +0630

    added custom resource

--- spring-boot-demo/spring-data-demo-rest/src/main/java/com/spring/data/demo/repo/EmployeeRepository.java
+++ spring-boot-demo/spring-data-demo-rest/src/main/java/com/spring/data/demo/repo/EmployeeRepository.java
@@ -1,8 +1,10 @@
 package com.spring.data.demo.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.spring.data.demo.entity.Employee;
 
+@RepositoryRestResource(path = "teams")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
